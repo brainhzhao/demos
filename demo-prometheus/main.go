1ac27f8d452101dbce093c55f89f50c34e32e317
brainzhao <brainzhao@tencent.com>
2023-02-13 17:27:30 +0800

    --init demo code--

---
+++ demo-prometheus/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"time"
+)
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+func main() {
+	recordMetrics()
+	http.Handle("/metrics", promhttp.Handler())
+	err := http.ListenAndServe(":8198", nil)
+	if err != nil {
+		return
+	}
+}
